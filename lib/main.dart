467ae941a96e2fe01d3012b1f3c5d10ac500e745
Mohib <mohibsaleem354@gmail.com>
2024-03-14 10:49:23 +0500

    conflicts resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:test_app/screens/auth/login_screen.dart';
-import 'package:test_app/screens/auth/otp_screen.dart';
-import 'package:test_app/screens/auth/registerd_screen.dart';
-import 'package:test_app/screens/learn_tutorials.dart';
-import 'package:test_app/screens/password_changed_screen.dart';
 import 'package:test_app/screens/profile_setup_screen.dart';
-import 'package:test_app/screens/splash.dart';
-import 'package:test_app/screens/welcome_screen.dart';
-=======
-import 'package:test_app/screens/profile_setup_screen.dart';
->>>>>>> 46042754cfbc3fc44b474fac7307ad946dc0e2d2
+import 'package:test_app/screens/profile_setup_screen_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,10 +15,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'SignVox',
         theme: ThemeData(),
-<<<<<<< HEAD
-        home: PasswordChangedScreen());
-=======
-        home: const ProfileSetupScreen());
->>>>>>> 46042754cfbc3fc44b474fac7307ad946dc0e2d2
+        home: const ProfileSetupScreen2());
   }
 }
